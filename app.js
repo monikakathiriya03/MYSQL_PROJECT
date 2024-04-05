0e3cbfeb4e0cef38f0f7ca8d76eaa9bd4e926e07
monika kathiriya <kathiriyamonika27@gmail.com>
2024-04-05 08:46:40 +0530

    05-04-2024

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'MONIKA@123',
+    database: 'node_mysql'
+});
+
+connection.connect(() => {
+    console.log('MySql is Connected!');
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Express!');
+});
+
+app.post('/api/user', (req, res) => {
+    const{ firstName, lastName, email, password, mobileNo} = req.body;
+    let user = {
+        firstName, lastName, email, password, mobileNo
+    };
+    connection.query('INSERT INTO user SET?', user, (err, data) => {
+        if(err) 
+        res.json(err);
+    else 
+    res.json(data);
+    });
+});
+
+app.get('/api/user/:id', (req, res) => {
+    let id = req.params.id;
+    console.log(id);
+    connection.query('select * from user where firstName = '+ `"${id}"`, (err, data) => {
+        if(err) 
+        res.json(err);
+    else
+    res.json(data);
+    })
+});
+
+app.listen(1919,() => {
+    console.log('Server Start at http://localhost:1919');
+});
